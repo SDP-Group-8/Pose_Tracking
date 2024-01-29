21b0f00865312e05d6bce26599586db18774b873
AlexWebb03 <alexanderwebb03@gmail.com>
2024-01-29 11:45:36 +0000

    setup project + demo

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Pose_Tracking
+First attempt at getting pose tracking setup and working
+
+# Quick Startup
+
+Warning: This works on mine currently but you might be missing prerequisites which I have on my machine.
+
+1. Install the project locally
+2. Get the model file
+
+```
+pip install --editable .
+wget https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task
+```
+
+# Scripts
+
+```
+estimate-video <video_source> -live
+estimate-image <image_path>
+```
+
+# Useful Links
+https://developers.google.com/mediapipe/solutions/vision/pose_landmarker
+https://developers.google.com/mediapipe/solutions/vision/pose_landmarker/python
---
+++ src/pose_estimation/capture_device.py
@@ -0,0 +1,38 @@
+import cv2
+from datetime import datetime
+
+class CaptureDevice:
+    def __init__(self, filepath: str, live: bool = False):
+        self.device = cv2.VideoCapture(filepath)
+        self.live = live
+        self.init_time = datetime.now()
+
+    def get_timestamp(self) -> datetime:
+        '''
+        Find the timestamp in milliseconds from the start of the video
+        '''
+        return datetime.now() - self.init_time if self.live else self.device.get(cv2.CAP_PROP_POS_MSEC)
+
+    def is_opened(self) -> bool:
+        '''
+        Determine whether the stream could be opened
+        '''
+        return self.device.isOpened()
+
+    def read(self):
+        '''
+        Read the next frame from the input device
+        '''
+        return self.device.read()
+    
+    def get_width(self) -> int:
+        '''
+        Get width of the video stream in pixels
+        '''
+        return self.device.get(3)
+    
+    def get_height(self) -> int:
+        '''
+        Get height of the video stream in pixels
+        '''
+        return self.device.get(4)
