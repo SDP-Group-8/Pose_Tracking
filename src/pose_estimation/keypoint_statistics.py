eead01d2a3eb7611af6f78f93b36b35c727b45e8
Jan Okul <janokul92@gmail.com>
2024-01-31 16:04:17 +0000

    Converted to NumPy

--- src/pose_estimation/keypoint_statistics.py
+++ src/pose_estimation/keypoint_statistics.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 from keypoints import Keypoints
 from mediapipe.tasks.python.components.containers.landmark import NormalizedLandmark
@@ -20,12 +21,12 @@ class KeypointStatistics:
     l_hip_r_hip_r_knee: float
     r_hip_r_knee_r_ankle: float
 
-    def to_list(self) -> [float]:
+    def to_list(self) -> 'np.ndarray':
         """
         Converts class into a list of angles.
         :return: List of key angles.
         """
-        return [
+        return np.array([
             self.r_shoulder_l_shoulder_l_elbow,
             self.l_shoulder_l_elbow_l_wrist,
             self.l_shoulder_r_shoulder_r_elbow,
@@ -34,7 +35,7 @@ class KeypointStatistics:
             self.l_hip_l_knee_l_ankle,
             self.l_hip_r_hip_r_knee,
             self.r_hip_r_knee_r_ankle
-        ]
+        ])
 
     @classmethod
     def from_keypoints(cls, keypoints: Keypoints) -> 'KeypointStatistics':
@@ -47,48 +48,48 @@ class KeypointStatistics:
         r_shoulder_l_shoulder_l_elbow = KeypointStatistics.calculate_angle(
             keypoints.right_shoulder,
             keypoints.left_shoulder,
-            keypoints.l_shoulder_l_elbow_l_wrist
+            keypoints.left_elbow
         )
 
         l_shoulder_l_elbow_l_wrist = KeypointStatistics.calculate_angle(
             keypoints.left_shoulder,
-            keypoints.l_shoulder_l_elbow_l_wrist,
+            keypoints.left_elbow,
             keypoints.left_wrist
         )
 
         l_shoulder_r_shoulder_r_elbow = KeypointStatistics.calculate_angle(
             keypoints.left_shoulder,
             keypoints.right_shoulder,
-            keypoints.r_shoulder_r_elbow_r_wrist
+            keypoints.right_elbow
         )
 
         r_shoulder_r_elbow_r_wrist = KeypointStatistics.calculate_angle(
             keypoints.right_shoulder,
-            keypoints.r_shoulder_r_elbow_r_wrist,
+            keypoints.right_elbow,
             keypoints.right_wrist
         )
 
         r_hip_l_hip_l_knee = KeypointStatistics.calculate_angle(
-            keypoints.l_hip_r_hip_r_knee,
-            keypoints.r_hip_l_hip_l_knee,
-            keypoints.l_hip_l_knee_l_ankle
+            keypoints.right_hip,
+            keypoints.left_hip,
+            keypoints.left_knee
         )
 
         l_hip_l_knee_l_ankle = KeypointStatistics.calculate_angle(
-            keypoints.r_hip_l_hip_l_knee,
-            keypoints.l_hip_l_knee_l_ankle,
+            keypoints.left_hip,
+            keypoints.left_knee,
             keypoints.left_ankle
         )
 
         l_hip_r_hip_r_knee = KeypointStatistics.calculate_angle(
-            keypoints.r_hip_l_hip_l_knee,
-            keypoints.l_hip_r_hip_r_knee,
-            keypoints.r_hip_r_knee_r_ankle
+            keypoints.left_hip,
+            keypoints.right_hip,
+            keypoints.right_knee
         )
 
         r_hip_r_knee_r_ankle = KeypointStatistics.calculate_angle(
-            keypoints.l_hip_r_hip_r_knee,
-            keypoints.r_hip_r_knee_r_ankle,
+            keypoints.right_hip,
+            keypoints.right_knee,
             keypoints.right_ankle
         )
 
@@ -124,17 +125,17 @@ class KeypointStatistics:
         yc = point_c.y
 
         # Calculate vectors AB and BC
-        ab = (xb - xa, yb - ya)
-        bc = (xc - xb, yc - yb)
+        ab = np.array([xb - xa, yb - ya])
+        bc = np.array([xc - xb, yc - yb])
 
         # Calculate dot product of AB and BC
-        dot_product = ab[0] * bc[0] + ab[1] * bc[1]
+        dot_product = np.dot(ab, bc)
 
         # Calculate magnitudes of AB and BC
-        magnitude_AB = math.sqrt(ab[0] ** 2 + ab[1] ** 2)
-        magnitude_BC = math.sqrt(bc[0] ** 2 + bc[1] ** 2)
+        magnitude_AB = np.linalg.norm(ab)
+        magnitude_BC = np.linalg.norm(bc)
 
         # Calculate cosine of the angle
         cos = dot_product / (magnitude_AB * magnitude_BC)
 
-        return math.degrees(math.acos(cos))
+        return math.acos(cos)
