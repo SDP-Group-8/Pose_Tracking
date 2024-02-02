53d65c06c767d10896e8262d43129d9f65263118
joshuasicw0818 <joshuasicw@gmail.com>
2024-02-02 01:14:38 +0000

    implemented optional weighted scoring

--- src/pose_estimation/scoring/euclidean_score.py
+++ src/pose_estimation/scoring/euclidean_score.py
@@ -6,14 +6,23 @@ import numpy as np
 class EuclideanScore(Score):
 
     # overriding abstract method 
-    def compute_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics) -> float:
+    def compute_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics, weights: np.ndarray = None) -> float:
         """
         Returns the average euclidean distance between 
         the landmarks of the two keypoints
+        :param first_keypoints: first set of keypoints
+        :param second_keypoints: second set of keypoints
+        :param weights: weights to apply to each distance
+        :return: euclidean score
         """
         first_landmarks = np.array(first_keypoints.keypoints.normalized_landmarks)
         second_landmarks = np.array(second_keypoints.keypoints.normalized_landmarks)
 
-        return np.mean(np.abs(first_landmarks - second_landmarks))
+        dist_difference = np.abs(first_landmarks - second_landmarks)
+        if weights is not None:
+            dist_difference = dist_difference * weights
+
+        return np.mean(dist_difference)
+
 
     
