6f704d4bb2f36a4e52ed8f8b48382e4013eebad7
joshuasicw0818 <joshuasicw@gmail.com>
2024-02-07 00:37:02 +0000

    Added scaled score method

--- src/pose_estimation/scoring/angle_score.py
+++ src/pose_estimation/scoring/angle_score.py
@@ -1,3 +1,4 @@
+from math import exp
 from pose_estimation.scoring.score import Score
 from pose_estimation.keypoint_statistics import KeypointStatistics
 import numpy as np
@@ -19,3 +20,25 @@ class AngleScore(Score):
             angle_diff = angle_diff * weights
 
         return np.mean(angle_diff)
+    
+    def compute_scaled_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics, weights: np.ndarray = None) -> float:
+        """
+        Returns the scaled score based on angle difference between
+        the two keypoints
+        :param first_keypoints: first set of keypoints
+        :param second_keypoints: second set of keypoints
+        :param weights: weights to apply to each angle
+        :return: scaled angle score
+        """
+        angle_diff = np.abs(second_keypoints.to_numpy() - first_keypoints.to_numpy())
+        angle_diff = np.rad2deg(angle_diff)
+
+        L = 200 # max score * 2
+        k = -0.0732 # logistic growth rate
+
+        log_func = lambda x: L / (1 + exp(-k*x)) 
+        log_score = [log_func(angle) for angle in angle_diff] # apply logistic function to each angle
+
+        if weights is not None:
+            log_score = log_score * weights
+        return np.mean(log_score)
