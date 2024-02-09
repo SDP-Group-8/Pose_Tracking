5b6de3b806b8e7de5e1a1f1e0fa485caa098d877
joshuasicw0818 <joshuasicw@gmail.com>
2024-02-09 14:05:03 +0000

    Changed the scale score stuff

--- src/pose_estimation/scoring/angle_score.py
+++ src/pose_estimation/scoring/angle_score.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class AngleScore(Score):
     # overriding abstract method 
-    def compute_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics, weights: np.ndarray = None) -> float:
+    def compute_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics, weights: np.ndarray = None, isScaled : bool = False) -> float:
         """
         Returns the average angle difference between
         the two keypoints
@@ -19,26 +19,20 @@ class AngleScore(Score):
         if weights is not None:
             angle_diff = angle_diff * weights
 
+        score = np.mean(angle_diff)
+        if isScaled:
+            score =  AngleScore.scale_score(score)
         return np.mean(angle_diff)
     
-    def compute_scaled_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics, weights: np.ndarray = None) -> float:
+    def scale_score(angle_diff : float) -> float:
         """
-        Returns the scaled score based on angle difference between
-        the two keypoints
-        :param first_keypoints: first set of keypoints
-        :param second_keypoints: second set of keypoints
-        :param weights: weights to apply to each angle
-        :return: scaled angle score
+        Returns the scaled score of the angle difference
+        :param angle_diff: angle difference
+        :return: scaled score
         """
-        angle_diff = np.abs(second_keypoints.to_numpy() - first_keypoints.to_numpy())
-        angle_diff = np.rad2deg(angle_diff)
+        angle_diff = np.abs(angle_diff)
 
         L = 200 # max score * 2
         k = -0.0732 # logistic growth rate
 
-        log_func = lambda x: L / (1 + exp(-k*x)) 
-        log_score = [log_func(angle) for angle in angle_diff] # apply logistic function to each angle
-
-        if weights is not None:
-            log_score = log_score * weights
-        return np.mean(log_score)
+        return L / (1 + exp(-k*angle_diff))
