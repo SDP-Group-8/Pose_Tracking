1fb78896533f9070d7a9b59db4f1e016df73cd94
joshuasicw0818 <joshuasicw@gmail.com>
2024-02-12 12:40:38 +0000

    Updated abstract score class

--- src/pose_estimation/scoring/score.py
+++ src/pose_estimation/scoring/score.py
@@ -4,6 +4,18 @@ from pose_estimation.keypoint_statistics import KeypointStatistics
 
 
 class Score(ABC):
+    #implement abstract method based on the AngleScore and EuclideanScore classes, the compute_score, scale_score and compute_each_score funcs
     @abstractmethod
-    def compute_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics, weights: np.ndarray = None) -> float:
+    def compute_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics, 
+                      weights: np.ndarray = None, isScaled : bool = False) -> float:
         pass
+
+    @abstractmethod
+    def compute_each_score(first_keypoints: KeypointStatistics, second_keypoints: KeypointStatistics, 
+                           weights: np.ndarray = None, isScaled : bool = False) -> list[float]:
+        pass
+
+    @abstractmethod
+    def scale_score(score : float) -> float:
+        pass
+    
